7672aa686ac93a86bde3be509b8fdecce05abf74
MISS <rrbruss001@gmail.com>
2023-09-21 20:52:12 +0800

    Initial commit

---
+++ app/src/main/java/ph/edu/auf/Ciscoactivity/MainActivity.java
@@ -0,0 +1,6 @@
+package ph.edu.auf.Ciscoactivity;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
---
+++ app/src/main/java/ph/edu/auf/Ciscoactivity/MainActivity.kt
@@ -0,0 +1,16 @@
+package ph.edu.auf.ciscoactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import...
+class motivationalmain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.Layout.motivational_main)
+
+        motivationalmain()
+    }
+    private fun motivational_main(){
+
+
+    }
+}
